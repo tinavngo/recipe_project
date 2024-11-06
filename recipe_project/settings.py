c252f38fcd1d788716dd4eee88fde8b2afa52156
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-11-05 18:09:37 -0800

    update heroku config

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -125,4 +125,5 @@ LOGGING = {
 }
 
 # Heroku: Update database configuration from $DATABASE_URL.
-django_heroku.settings(locals())
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
