df71844938947b28a3a3a3add8439a8cd5a68e57
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-11-08 15:41:11 -0800

    correct naming for container styling

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -94,10 +94,11 @@ USE_TZ = True
 LOGIN_URL = '/login/'
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
